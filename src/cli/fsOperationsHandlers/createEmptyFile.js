78e7b916b7a96b2412dded30a5ae7fc9e8dbeaa1
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-10-14 22:27:01 +0300

    refactor createEmptyFile

--- src/cli/fsOperationsHandlers/createEmptyFile.js
+++ src/cli/fsOperationsHandlers/createEmptyFile.js
@@ -3,13 +3,17 @@ import path from 'node:path';
 import { printErrorToConsole } from '../../utils/printErrorToConsole.js';
 import { CREATION_ERROR } from '../../consts/errorMessages.js';
 import { printCurrentWorkingDir } from '../../utils/printCurrentWorkingDir.js';
+import { COLORS } from '../../consts/colors.js';
 
 export async function createFile(fileName) {
-    try {
-        const fileToCreatePath = path.join(process.cwd(), fileName)
-        await fs.writeFile(fileToCreatePath, '', { flag: 'wx' })
-        printCurrentWorkingDir();
-  } catch {
-    printErrorToConsole(CREATION_ERROR)
-  }
+	try {
+		const fileToCreatePath = path.join(process.cwd(), fileName);
+		await fs.writeFile(fileToCreatePath, '', { flag: 'wx' });
+		console.log(
+			`${COLORS.success}${fileName} was successfully created! 👏\n${COLORS.reset}`
+		);
+		printCurrentWorkingDir();
+	} catch (err) {
+		printErrorToConsole(`Operation failed! ${err.message}`);
+	}
 }
